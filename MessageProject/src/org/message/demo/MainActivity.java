f5e780449a437aac8bfd5f1c300c8068777a93ca
王跃斌 <395044952@qq.com>
2014-09-23 22:53:33 +0800

    继续加载
    加载文件....

--- MessageProject/src/org/message/demo/MainActivity.java
+++ MessageProject/src/org/message/demo/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);//加载文件...
+		setContentView(R.layout.activity_main);//加载文件...继续加载文件...
 	}
 
 	@Override
